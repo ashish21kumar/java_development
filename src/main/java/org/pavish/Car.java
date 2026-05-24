a05ba4b064785720821fadff0e07206a192accd2
Ashish Kumar <2103ashish@gmail.com>
2026-05-24 14:50:37 +0530

    Encapsulation overview

---
+++ src/main/java/org/pavish/Car.java
@@ -0,0 +1,58 @@
+package org.pavish;
+
+public class Car {
+    private String color;
+    private String model;
+    private String brand;
+    private int speed;
+    private int year;
+
+
+    public void accelerate(int increment){
+        speed += increment;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void brake(int decrement){
+        speed -= decrement;
+    }
+}
